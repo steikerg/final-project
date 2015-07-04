65cb37ef454b4110e6eba8c8b61dd5f94f25be99
Gene Steiker <gene.steiker@gmail.com>
2015-07-04 15:31:17 -0400

    Populate page title from JSON data

--- js/portfolio-builder.js
+++ js/portfolio-builder.js
@@ -2,6 +2,7 @@
 // Site-info - JSON object
 //*****************************************************************
 var siteInfo = {
+    "pageTitle": "Portfolio - G Steiker",
     "logo" : "images/orange.png",
     "name" : "Gene Steiker",
     "role" : "Web Developer",
@@ -20,7 +21,7 @@ var siteInfo = {
             "label": "phone",
             "value": "973 876-2345"
         },
-                {
+        {
             "label": "location",
             "value": "Caldwell, NJ"
         }
@@ -35,25 +36,31 @@ var projects = {
       {
         "screenshot": "http://lorempixel.com/g/360/220/sports",
         "alt": "Appify Screenshot",
-        "short-title": "Appify",
+        "title": "Appify",
+        "url": "https://github.com",
         "long-title": "Appify Application",
         "modal-image": "http://lorempixel.com/g/360/220/sports",
+        "modal-alt-text": "Appify Screenshot",
         "description": "This was my second project in this HTML and CSS class. I learned a lot about HTML and CSS."
         },
       {
         "screenshot": "http://placebear.com/360/220",
         "alt": "Sunflower Screenshot",
         "title": "Sunflower",
+        "url": "https://github.com",
         "long-title": "Sunflower Smoke Shop",
         "modal-image": "http://placebear.com/360/220",
+        "modal-alt-text": "Sunflower Screenshot",
         "description": "Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.<p><p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. <p>Maecenas sed diam eget risus varius blandit sit amet non magna. Donec id elit non mi porta gravida at eget metus. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit."
         },
       {
         "screenshot": "images/Screenprint-FinalProject.png",
         "alt": "Steiker Portfolio Screenshot",
         "title": "Portfolio",
+        "url": "https://github.com/steikerg",
         "long-title": "Gene Steiker Portfolio",
         "modal-image": "images/Screenprint-FinalProject.png",
+        "modal-alt-text": "Gene Steiker Portfolio Screenshot",
         "description": "Does this thumbnail look familiar? It's the web page you are visiting right now. Originally developed for a class, I refined the page and put it into use. This page was built using the Bootstrap library. It is populated with information stored in JSON data objects."
         }      
     ]
@@ -66,6 +73,7 @@ var projects = {
 var HTMLheaderName = '<h1 class="text-thin title-super text-loud">%data%</h1>';
 var HTMLheaderRole = '<h4>%data%</h4>';
 var HTMLlogo = '<img class="title-logo" src="%data%" alt="GDS logo" >'
+var HTMLtitle = '<title>%data%</title>'
 
 var halfBaked = '';
 var HTMLcontactItem = '<li class="flex-item"><span class="text-muted">%label%</span><span class="text-padding">%data%</span></li>'
